5134d52fb5f71d075806ad79d83eadabd7137f52
Matpas97 <passarellim22@gmail.com>
2022-11-16 16:17:15 +0100

    first commit

---
+++ laravel_auth/resources/js/views/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+       <ExampleComponentVue />
+    </div>
+    </template>
+    
+    <script>
+    import ExampleComponentVue from '../js/components/ExampleComponent.vue';
+    
+    
+    
+    
+    export default{
+        name: 'App',
+        components: { ExampleComponentVue }
+    }
+    </script>
+    
+    <style scoped lang="scss">
+    
+    
+    </style>
